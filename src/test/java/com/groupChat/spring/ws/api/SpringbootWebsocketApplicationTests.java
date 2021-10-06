9abc2de3057c382f43b7b8e9726308f62cc61e22
Ansari <charigardash@gmail.com>
2021-10-06 11:53:30 +0530

    Lets Chat
    Group Chat

---
+++ src/test/java/com/groupChat/spring/ws/api/SpringbootWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupChat.spring.ws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
